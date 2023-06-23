94a0edb5f7d32da21c58b22dce24e047fc8439a3
tati-b <116196864+tati-b@users.noreply.github.com>
2023-06-23 15:04:01 -0400

    Update q2.py

--- q2.py
+++ q2.py
@@ -12,17 +12,16 @@ output_test = np.array([1, 1, 0, 0, 1, 1, 1, 1])
 
 # Creating the Keras Sequential model
 model = Sequential([
-    Dense(12, input_shape=(5,), activation='relu'),
-    Dense(8, activation='relu'),
-    Dense(7, activation='relu'),
+    Dense(8, input_shape=(5,), activation='relu'),
+    # Dense(8, activation='relu'),
+    Dense(5, activation='relu'),
     Dense(1, activation='sigmoid'),
 ])
-
 # Compiling the created model
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'],) #sgd 
 
-# Training the model by iterating on the data in batches of 50 smaples -- before it was 10 and accuracy for predicted output was 62.50
-model.fit(input, output, epochs=150, batch_size=50)
+# Training the model by iterating on the data in batches of 50 samples -- beffore it was 10 and accuracy for predicted output was 62.50
+model.fit(input, output, epochs=180, batch_size=35)
 
 # Evaluating the model
 _, accuracy = model.evaluate(input, output)
